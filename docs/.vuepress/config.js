e726af22a8435a0e0bce54880f411fff28a3ebe7
AqingCyan <xuekaiqi@creatshare.com>
2019-03-20 21:00:41 +0800

    :art:代码重构

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'ES6学习指南',
+  description: 'ES6常用特性内容演示与说明',
+  themeConfig: {
+    repo: 'AqingCyan/Making-ES6',
+    editLinks: true,
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdated: '上次更新',
+    sidebar: [
+      ['/','写在文档之前'],
+      ['/ES6book/01.let与const','let与const以及块级作用域'],
+      ['/ES6book/02.解构赋值', '解构赋值'],
+      ['/ES6book/03.字符串拓展', '字符串的拓展']
+    ]
+  },
+  base: '/Making-ES6/'
+}
